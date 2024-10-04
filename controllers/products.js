e6f56a2b460286d77a2e10daceb09b6f383a49b0
Isha Dewangan <dewanganisha05@gmail.com>
2024-10-03 22:25:35 -0700

    pagination added

--- controllers/products.js
+++ controllers/products.js
@@ -34,17 +34,34 @@ const getAllProducts = async (req, res) => {
     apiData = apiData.select(selectFix);
   }
 
+  
+  
+  let page = Number(req.query.page) || 1
+  let limit = Number(req.query.limit) || 10 
+  let skip = (page - 1) * limit
+
+  // page  = 2
+  // limit = 3
+  // skip = 1 * 3 = 3
+
+  apiData = apiData.skip(skip).limit(limit);
+
+
+
   console.log(queryObject);
 
-  const myData = await apiData;
-  res.status(200).json({ myData });
+  const Products = await apiData;
+  res.status(200).json({ Products, nbHits: Products.length });
 };
 
+
 const getAllProductsTesting = async (req, res) => {
-  const myData = await Product.find(req.query).select("name price");
+  console.log("Product Testing", req.query)  
+  const Products = await Product.find(req.query).skip(2) 
+  // const Products = await Product.find(req.query).select("name price");
   // sort=name,price
 
-  res.status(200).json({ myData });
+  res.status(200).json({ Products });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
